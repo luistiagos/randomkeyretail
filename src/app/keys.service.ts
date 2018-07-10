33510713456261120b729cd89e23307f9e089412
Luis Tiago Andrighetto Hablich - SUPDE/DECTA/DE903 <luistiago.andrighetto@gmail.com>
2018-07-10 18:45:20 -0300

    sdfdsgsd

--- src/app/keys.service.ts
+++ src/app/keys.service.ts
@@ -7,11 +7,10 @@ export class KeysService {
 
   constructor(private http: HttpClient) { }
 
-  
   getReedemCodeKeys(reedemCode:string) {
     let strparams = "{code:'" + reedemCode +"'}";
     let url = 'https://api.mlab.com/api/1/databases/randomkeysbox/collections/reedemcodes?q='+strparams;
-    url += '&apiKey=j7xm963vICVfSSop9KrwWraF8fMMf9_w';
+    url += '&apiKey=' + Settings.API_KEY;
     return this.http.get(url);
   }
 }
